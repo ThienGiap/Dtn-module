8b4bf1ce82659760ad98388c4b2a95b8c6d2e53f
ThienGiap <nguyenthiengiap04@gmail.com>
2025-02-19 13:51:51 +0700

    Create models

---
+++ app/code/Dtn/Office/Model/Department.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Dtn\Office\Model;
+
+use Magento\Framework\Model\AbstractModel;
+
+class Department extends AbstractModel
+{
+    protected function _construct()
+    {
+        $this->_init(\Dtn\Office\Model\ResourceModel\Department::class);
+    }
+}
