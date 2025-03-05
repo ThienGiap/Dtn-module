231564f6e3c2cbca2015ed6667d3eaf43fc68f1c
ThienGiap <nguyenthiengiap04@gmail.com>
2025-03-05 15:01:51 +0700

    Create filter Employee and Department

---
+++ app/code/Dtn/Office/Ui/Component/DataProvider.php
@@ -0,0 +1,82 @@
+<?php
+
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+namespace Dtn\Office\Ui\Component;
+
+use Magento\Framework\Api\Filter;
+use Magento\Framework\Api\FilterBuilder;
+use Magento\Framework\Api\Search\SearchCriteriaBuilder;
+use Magento\Framework\App\RequestInterface;
+use Magento\Framework\View\Element\UiComponent\DataProvider\Reporting;
+
+class DataProvider extends \Magento\Framework\View\Element\UiComponent\DataProvider\DataProvider
+{
+    /**
+     * @var AddFilterInterface[]
+     */
+    private $additionalFilterPool;
+
+    /**
+     * @param string $name
+     * @param string $primaryFieldName
+     * @param string $requestFieldName
+     * @param Reporting $reporting
+     * @param SearchCriteriaBuilder $searchCriteriaBuilder
+     * @param RequestInterface $request
+     * @param FilterBuilder $filterBuilder
+     * @param array $meta
+     * @param array $data
+     * @param array $additionalFilterPool
+     * @SuppressWarnings(PHPMD.ExcessiveParameterList)
+     */
+    public function __construct(
+        $name,
+        $primaryFieldName,
+        $requestFieldName,
+        Reporting $reporting,
+        SearchCriteriaBuilder $searchCriteriaBuilder,
+        RequestInterface $request,
+        FilterBuilder $filterBuilder,
+        array $meta = [],
+        array $data = [],
+        array $additionalFilterPool = []
+    ) {
+        parent::__construct(
+            $name,
+            $primaryFieldName,
+            $requestFieldName,
+            $reporting,
+            $searchCriteriaBuilder,
+            $request,
+            $filterBuilder,
+            $meta,
+            $data
+        );
+
+        $this->meta = array_replace_recursive($meta);
+        $this->additionalFilterPool = $additionalFilterPool;
+    }
+
+    /**
+     * Add Filter
+     *
+     * @param Filter $filter
+     * @return void
+     */
+    public function addFilter(Filter $filter): void
+    {
+        if (!empty($this->additionalFilterPool[$filter->getField()])) {
+            $this->additionalFilterPool[$filter->getField()]->addFilter($this->searchCriteriaBuilder, $filter);
+        } else {
+            if ($filter->getField() === 'department_id') {
+                $filter->setField('main_table.department_id');
+            }
+
+            parent::addFilter($filter);
+        }
+    }
+}
